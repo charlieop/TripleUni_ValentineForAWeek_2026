11cbcc8e4d677ea0847a4ee9c441c0d0dca6e61d
charlieop <spchanae@connect.ust.hk>
2026-02-08 16:39:31 -0500

    changed scoring

--- backend/main/autograder/autograder.py
+++ backend/main/autograder/autograder.py
@@ -2,8 +2,9 @@ import modal
 
 from ..models import Match, Task
 from ..logger import CustomLogger
+from ..configs import AvtivityDates
 
-from .autograder_utils import batched, get_mission_prompt, get_all_base64_images, save_response_to_tasks
+from .autograder_utils import batched, get_mission_prompt, get_all_base64_images, save_response_to_tasks, get_tripleuni_post_record, remove_empty_payload
 
 logger = CustomLogger("autograder")
 
@@ -38,7 +39,7 @@ class Autograder():
         else:
             raise ValueError(f"Invalid day: {self.day}")
     
-    def query_batch(self, tasks: list[Task]) -> list[dict]:
+    def llm_batch_grading(self, tasks: list[Task]) -> list[dict]:
         logger.info(f"Querying batch of {len(tasks)} tasks")
         for task_batch in batched(tasks, self.batch_size):
             try:    
@@ -48,22 +49,47 @@ class Autograder():
                         "text": task.submit_text or "这是我今天的提交内容"
                     } for task in task_batch
                 }
+                payload = remove_empty_payload(payload, self.day)
                 response = self.instance.run_batch_query_by_base64.remote(payload, system_prompt=get_mission_prompt(self.day))
                 save_response_to_tasks(self.day, response, self.use_thinking)
-                logger.info(f"Queried batch of {len(task_batch)} tasks")
+                logger.info(f"Queried batch of {len(task_batch)} tasks: {task_batch}")
             except Exception as e:
                 logger.error(f"Error while querying batch {task_batch}: {e}")
                 continue
         logger.info(f"Queried all tasks")
         
+    def query_tripleuni_batch(self, tasks: list[Task], score: int = 10) -> list[dict]:
+        release_time_unix = int(AvtivityDates.MISSION_RELEASE_DAY(self.day).timestamp())
+        end_time_unix = int(AvtivityDates.MISSION_SUBMIT_END_DAY(self.day).timestamp())
+        logger.info(f"Querying tripleuni batch of {len(tasks)} tasks from {release_time_unix} to {end_time_unix}")
+        
+        for task in tasks:
+            try:
+                uni_task_completed = get_tripleuni_post_record(task, release_time_unix, end_time_unix)
+                if uni_task_completed:
+                    task.uni_score = score
+                    task.uni_review = "今日已在 Triple Uni 一周CP 板块发帖"
+                else:
+                    task.uni_score = 0
+                    task.uni_review = "今日未在 Triple Uni 一周CP 板块发帖"
+            except Exception as e:
+                task.uni_score = 0
+                task.uni_review = "Triple Uni 评分失败, 请联系你的Mentor"
+                logger.error(f"Error while querying tripleuni for task {task}: {e}")
+                continue
+            task.save()
+        logger.info(f"Queried all tasks")
+        return tasks
+    
     def grade_day1_batch(self, tasks: list[Task]) -> list[dict]:
         logger.info(f"Grading day 1 batch of {len(tasks)} tasks")
-        self.query_batch(tasks)
+        self.query_tripleuni_batch(tasks, score=15)
+        self.llm_batch_grading(tasks)
         for task in tasks:
-            if task.match.name != "取个组名吧!":
+            if task.match.name not in ["取个组名吧!", "取一个组名吧!"]:
                 task.bonus_score += 10
                 task.bonus_review = str(task.bonus_review) + "\n任务: 我们是XXX 已完成, 里程+10"
-            task.uni_review = "Triple Uni 暂未评分"
+            task.scored = True
             task.save()
         logger.info(f"Graded day 1 batch of {len(tasks)} tasks")
         return tasks
--- backend/main/autograder/autograder_utils.py
+++ backend/main/autograder/autograder_utils.py
@@ -1,9 +1,21 @@
 import base64
 from itertools import islice
 import json
+import requests
+
+from django.conf import settings
 
 from ..models import Mission, Task
+from ..logger import CustomLogger
 
+API_URL = "https://eo.api.tripleuuunnniii.com/v4/valentine/getusercontent.php"
+
+with open(settings.BASE_DIR / "SECRETS.json") as f:
+    secrets = json.load(f)
+    UNI_APPID = secrets["UNI_APPID"]
+    UNI_APPKEY = secrets["UNI_APPKEY"]
+
+logger = CustomLogger("autograder_utils")
 
 def batched(iterator, batch_size):
     it = iter(iterator)
@@ -28,12 +40,32 @@ def get_all_base64_images(task: Task):
 def save_response_to_tasks(day: int, response: dict, use_thinking: bool):
     for match_id, text in response.items():
         task = Task.objects.get(match_id=match_id, day=day)
-        if use_thinking:
-            thinking_process, score_json = text.split("</think>")
-        else:
-            thinking_process, score_json = "未使用思考过程", text
         
-        score = json.loads(score_json)
+        try:
+            if use_thinking:
+                thinking_process, score_json = text.split("</think>")
+            else:
+                thinking_process, score_json = "未使用思考过程", text
+            score = json.loads(score_json)
+        except Exception as e:
+            logger.error(f"Error while saving response to tasks for task {task}: {e}")
+            score = {
+                "basic": {
+                    "score": 0,
+                    "reason": "模型评分失败, 请联系你的Mentor"
+                },
+                "bonus": {
+                    "score": 0,
+                    "reason": "模型评分失败, 请联系你的Mentor"
+                },
+                "daily": {
+                    "score": 0,
+                    "reason": "模型评分失败, 请联系你的Mentor"
+                },
+                "basic_completed": False
+            }
+            thinking_process = text
+        
         task.basic_completed = score.get("basic_completed", False)
         task.basic_score = score.get("basic", {}).get("score", 0)
         task.bonus_score = score.get("bonus", {}).get("score", 0)
@@ -44,6 +76,50 @@ def save_response_to_tasks(day: int, response: dict, use_thinking: bool):
         task.daily_review = score.get("daily", {}).get("reason", "")
 
         task.thinking_process = thinking_process
-
-        task.scored = True
         task.save()
+        
+def remove_empty_payload(payload: dict, day: int) -> dict:
+    empty_payload = {mid: data for mid, data in payload.items() if len(data["base64_images"]) == 0}
+    for mid in empty_payload.keys():
+        task = Task.objects.get(match_id=mid, day=day)
+        task.basic_completed = False
+        task.basic_score = 0
+        task.bonus_score = 0
+        task.daily_score = 0
+        task.basic_review = "未上传图片, 提交内容为空"
+        task.bonus_review = "未上传图片, 提交内容为空"
+        task.daily_review = "未上传图片, 提交内容为空"
+        task.thinking_process = "未上传图片, 提交内容为空"
+        task.save()
+        del payload[mid]
+    return payload
+        
+
+def get_tripleuni_post_record(task: Task, start_time_unix: int, end_time_unix: int) -> bool:
+    match = task.match
+    applicant1 = match.applicant1
+    applicant2 = match.applicant2
+    
+    if applicant1.linked_uni:
+        applicant1_email = applicant1.email
+        uni_task_completed = _query_tripleuni_post_record(applicant1_email, start_time_unix, end_time_unix)
+        if uni_task_completed:
+            return True
+        
+    if applicant2.linked_uni:
+        applicant2_email = applicant2.email
+        uni_task_completed = _query_tripleuni_post_record(applicant2_email, start_time_unix, end_time_unix)
+        if uni_task_completed:
+            return True
+            
+    return False
+
+def _query_tripleuni_post_record(email: str, start_time_unix: int, end_time_unix: int) -> bool:
+    payload = f"appid={UNI_APPID}&appkey={UNI_APPKEY}&user_email={email}&start_time={start_time_unix}&end_time={end_time_unix}"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    response = requests.request("POST", API_URL, data=payload, headers=headers)
+    data = response.json()
+    if "post_list" in data:
+        return len(data["post_list"]) > 0
+    else:
+        return False
